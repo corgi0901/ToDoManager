7df38bc09287b865d33503f3d782486b0e2745e0
Ryo <ryo.yoneyama.dev@gmail.com>
2017-05-21 23:12:55 +0900

    change target framework

--- ToDoManager/ToDoManager/Properties/AssemblyInfo.cs
+++ ToDoManager/ToDoManager/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("TaskListManager")]
+[assembly: AssemblyTitle("ToDoManager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TaskListManager")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("ToDoManager")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
